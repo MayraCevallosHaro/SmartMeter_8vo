ed54968a86d77c60789b0b7cc0b1f502d09f3eab
MayraCevallosHaro <mayra.cevallos2016@uteq.edu.ec>
2021-08-18 14:14:33 -0500

    Second

--- app/src/main/java/com/example/smartmeter_8vo/Home.java
+++ app/src/main/java/com/example/smartmeter_8vo/Home.java
@@ -1,14 +1,88 @@
 package com.example.smartmeter_8vo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private FirebaseAuth auth;
 
+    DatabaseReference mDataBase;
+
+    SignInButton btSignIn;
+    GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+        email = findViewById(R.id.inputUser);
+        password = findViewById(R.id.inputPassword);
+        login = findViewById(R.id.btnRegistro2);
+        auth = FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                loginUser(txt_email, txt_password);
+            }
+        });
+    }
+
+        private void loginUser(String email, String pass) {
+            auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        String id = auth.getCurrentUser().getUid();
+                        mDataBase.child("usuario").child(id).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+                                    String rol = snapshot.child("rol").getValue().toString();
+                                        startActivity(new Intent(Home.this, Menu.class));
+                                        finish();
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    } else {
+                        Toast.makeText(Home.this, "ERROR USER OR PASSWORD", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+
+
+
     }
 }
