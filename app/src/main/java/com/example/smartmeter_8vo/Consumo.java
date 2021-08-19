99da707741648a0c15418c29b038b9c18015af57
MayraCevallosHaro <mayra.cevallos2016@uteq.edu.ec>
2021-08-18 19:55:10 -0500

    Desing

---
+++ app/src/main/java/com/example/smartmeter_8vo/Consumo.java
@@ -0,0 +1,14 @@
+package com.example.smartmeter_8vo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Consumo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumo);
+    }
+}
---
+++ app/src/main/java/com/example/smartmeter_8vo/ReportPower.java
@@ -0,0 +1,14 @@
+package com.example.smartmeter_8vo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportPower extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_power);
+    }
+}
