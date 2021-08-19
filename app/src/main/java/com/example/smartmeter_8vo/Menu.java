d3bf4f813ec3908d0550cf07d6a416995bc3ad4d
MayraCevallosHaro <mayra.cevallos2016@uteq.edu.ec>
2021-08-18 19:15:13 -0500

    Arreglo

--- app/src/main/java/com/example/smartmeter_8vo/Menu.java
+++ app/src/main/java/com/example/smartmeter_8vo/Menu.java
@@ -1,14 +1,39 @@
 package com.example.smartmeter_8vo;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Menu extends AppCompatActivity {
+    private ImageButton salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        salir = (ImageButton) findViewById(R.id.btnCerrar);
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"search button is Clicked", Toast.LENGTH_LONG).show();
+                FirebaseAuth.getInstance().signOut();
+
+            }
+        });
+
     }
+
+
+
+
+
 }
